4e8dddc9570c2f1eb14d0b5688c9d07219475f02
Ignacio Martin <ignamartin361@gmail.com>
2023-03-19 10:30:22 -0300

    Actualizaciones del Proyecto

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,19 +5,11 @@ import {Link} from "react-router-dom";
 const Nav = ({onSearch}) => {
     return (
         <div className= {styled.Nav}>
-              {/* <div className={styled.contenedor}>
-      			  <p>Hola</p>
-			    <ul>
-      <li>Usuario</li>
-      <li>Bienvenido</li>
-				      <li>👋👋</li>
-			    </ul>
-      </div> */}
             <img src="https://hbomax-images.warnermediacdn.com/2022-08/tt.png?host=wme-hbomax-drupal-prod.s3.amazonaws.com"/>
         <Link to={"/Favorites"} className={styled.Link}>Favorites</Link>    
         <Link to = {"/home"} className={styled.Link} >Home</Link>  
         <Link to = {"/about"} className={styled.Link}>About</Link>
-        <Link to={"/"} className = {styled.Link} >Logout</Link>
+        <Link to={"/"} className = {styled.Link} >Login</Link>
         <SearchBar onSearch={onSearch} />
         </div>
     )
